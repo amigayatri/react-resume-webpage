42aa776762e04bc4e771d75ae500aacfb1623312
Ami Gayatri <miragayatri@gmail.com>
2024-09-25 02:52:23 -0300

    fix?

--- src/components/leetcode/HeatMap/HeatMap.styled.ts
+++ src/components/leetcode/HeatMap/HeatMap.styled.ts
@@ -4,10 +4,10 @@ import media from "../../../constants/medias"
 export const HeatMapWrapper = styled.section`
 	display: grid;
 	grid-template-columns: repeat(3, 1fr);
-	${media.max.md} {
-		grid-template-columns: repeat(1, 1fr);
-	}
 	${media.max.xl} {
 		grid-template-columns: repeat(2, 1fr);
+	}
+        ${media.max.md} {
+		grid-template-columns: repeat(1, 1fr);
 	}
 `
