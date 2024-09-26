76a5c99dd0b0438218d1c0f08de605c178bfda76
Ami Gayatri <miragayatri@gmail.com>
2024-09-26 16:25:18 -0300

    Fix warning preventing build

--- src/constants/pages.ts
+++ src/constants/pages.ts
@@ -3,7 +3,7 @@ import ResumeProps from "../pages/Resume/Resume.props"
 import PageProps from "../types/PageProps"
 import ColorsProps from "../pages/Colors/Colors.props"
 import ExtraLanguagesProps from "../pages/ExtraLanguages/ExtraLanguages.props"
-import LeetcodeProps from "../pages/Leetcode/leetcode.props"
+//import LeetcodeProps from "../pages/Leetcode/leetcode.props"
 
 export const pages: PageProps[] = [HomeProps, ResumeProps]
 
