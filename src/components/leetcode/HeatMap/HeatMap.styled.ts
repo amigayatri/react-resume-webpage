1e5d418dd9479af33b4bf607dc1b9eb97701c162
Ami Gayatri <miragayatri@gmail.com>
2024-09-25 23:25:03 -0300

    visual hotfix

--- src/components/leetcode/HeatMap/HeatMap.styled.ts
+++ src/components/leetcode/HeatMap/HeatMap.styled.ts
@@ -1,15 +1,8 @@
 import styled from "styled-components"
-import media from "../../../constants/medias"
 
 export const HeatMapWrapper = styled.section`
 	display: grid;
-	grid-template-columns: repeat(3, 1fr);
+	grid-template-columns: repeat(1, 1fr);
 	width: fit-content;
 	margin: 0 auto;
-	${media.max.xl} {
-		grid-template-columns: repeat(2, 1fr);
-	}
-	${media.max.lg} {
-		grid-template-columns: repeat(1, 1fr);
-	}
 `
