fc73ebfe78481c1994c6d8088acbfe708fcfc3b4
Ami Gayatri <miragayatri@gmail.com>
2024-10-06 21:34:54 -0300

    Update import

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,6 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector"
-import resources from "../public/i18n"
+import resources from "./languages"
 
 i18n.use(LanguageDetector).init({
 	fallbackLng: "en",
