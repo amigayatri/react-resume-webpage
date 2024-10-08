4378e9db5edcd47cbacf689419b9b13ee807aeda
Ami Gayatri <miragayatri@gmail.com>
2024-10-08 15:20:39 -0300

    Fix on mobile style

--- src/components/common/LanguageSelect/LanguageSelect.styled.ts
+++ src/components/common/LanguageSelect/LanguageSelect.styled.ts
@@ -1,10 +1,14 @@
 import styled from "styled-components"
+import media from "../../../constants/medias"
 
 export const LanguageSelectInput = styled.select`
 	font-size: 1.25rem;
 	background-color: ${({ theme }) => theme.black};
 	color: ${({ theme }) => theme.white};
 	border-color: ${({ theme }) => theme.black};
+ ${media.max.xs} {
+max-width: 200px
+}
 `
 
 export const LanguageOption = styled.option`
