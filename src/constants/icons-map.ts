3b7d98156399321cbf76737b66fb3698982b737e
Ami Gayatri <miragayatri@gmail.com>
2024-10-21 16:43:14 -0300

    ✨ Almost 100 new icons (#43)

--- src/constants/icons-map.ts
+++ src/constants/icons-map.ts
@@ -1,156 +1,354 @@
+import AlienIcon from "../icons/AlienIcon"
+import AncientGateIcon from "../icons/AncientGateIcon"
+import AncientPavilionIcon from "../icons/AncientPavilionIcon"
+import AtlasIcon from "../icons/AtlasIcon"
+import AxolotlIcon from "../icons/AxolotlIcon"
 import BaiduIcon from "../icons/BaiduIcon"
 import BankCardIcon from "../icons/BankCardIcon"
 import BarbieIcon from "../icons/BarbieIcon"
+import BastetIcon from "../icons/BastetIcon"
 import BeeIcon from "../icons/BeeIcon"
+import BeerIcon from "../icons/BeerIcon"
+import BikeIcon from "../icons/BikeIcon"
+import BowlIcon from "../icons/BowlIcon"
+import BrainIcon from "../icons/BrainIcon"
 import BrasilIcon from "../icons/BrasilIcon"
 import BrazilianCalendarIcon from "../icons/BrazilianCalendarIcon"
+import BreadIcon from "../icons/BreadIcon"
 import BrokenLinkIcon from "../icons/BrokenLinkIcon"
 import BugIcon from "../icons/BugIcon"
 import BurgerIcon from "../icons/BurgerIcon"
+import BusIcon from "../icons/BusIcon"
+import ButterflyIcon from "../icons/ButterflyIcon"
+import CSSIcon from "../icons/CSSIcon"
+import CactusIcon from "../icons/CactusIcon"
+import CakeIcon from "../icons/CakeIcon"
 import CalendarIcon from "../icons/CalendarIcon"
+import CandleIcon from "../icons/CandleIcon"
+import CapsuleIcon from "../icons/CapsuleIcon"
+import CarIcon from "../icons/CarIcon"
+import CatIcon from "../icons/CatIcon"
+import CaterpillarIcon from "../icons/CaterpillarIcon"
 import CedarIcon from "../icons/CedarIcon"
+import CentaurIcon from "../icons/CentaurIcon"
 import ChartIcon from "../icons/ChartIcon"
+import CigaretteIcon from "../icons/CigaretteIcon"
 import CircleIcon from "../icons/CircleIcon"
 import ClapperboardIcon from "../icons/ClapperboardIcon"
+import CloudIcon from "../icons/CloudIcon"
 import CodeIcon from "../icons/CodeIcon"
 import ColorFilterIcon from "../icons/ColorFilterIcon"
 import ComputerIcon from "../icons/ComputerIcon"
+import CrabIcon from "../icons/CrabIcon"
 import CrownIcon from "../icons/CrownIcon"
 import CryingIcon from "../icons/CryingIcon"
+import CupcakeIcon from "../icons/CupcakeIcon"
+import DNAIcon from "../icons/DNAIcon"
+import DiceIcon from "../icons/DiceIcon"
+import DiscIcon from "../icons/DiscIcon"
+import DjinnIcon from "../icons/DjinnIcon"
 import DownloadIcon from "../icons/DownloadIcon"
 import DrinkIcon from "../icons/DrinkIcon"
+import DuckIcon from "../icons/DuckIcon"
+import EarthIcon from "../icons/EarthIcon"
+import EgyptianCatIcon from "../icons/EgyptianCatIcon"
+import EgyptianPyramidsIcon from "../icons/EgyptianPyramidsIcon"
+import EgyptianSphinxIcon from "../icons/EgyptianSphinxIcon"
+import EgyptianWalkIcon from "../icons/EgyptianWalkIcon"
+import ElephantIcon from "../icons/ElephantIcon"
 import EmailIcon from "../icons/EmailIcon"
+import EmpathizeIcon from "../icons/EmpathizeIcon"
 import ErrorIcon from "../icons/ErrorIcon"
 import ExternalIcon from "../icons/ExternalIcon"
+import EyeOfHorusIcon from "../icons/EyeOfHorusIcon"
 import FacebookIcon from "../icons/FacebookIcon"
+import FilmIcon from "../icons/FilmIcon"
+import FireIcon from "../icons/FireIcon"
 import FirefoxIcon from "../icons/FirefoxIcon"
+import FlagIcon from "../icons/FlagIcon"
+import FlowerIcon from "../icons/FlowerIcon"
 import GamepadIcon from "../icons/GamepadIcon"
+import GenderlessIcon from "../icons/GenderlessIcon"
+import GhostIcon from "../icons/GhostIcon"
 import GitBranchIcon from "../icons/GitBranchIcon"
 import GithubIcon from "../icons/GithubIcon"
+import GlassesIcon from "../icons/GlassesIcon"
+import GlobeIcon from "../icons/GlobeIcon"
 import GoogleIcon from "../icons/GoogleIcon"
+import GraduationCapIcon from "../icons/GraduationCapIcon"
+import GreekSphinxIcon from "../icons/GreekSphinxIcon"
+import GreekTempleIcon from "../icons/GreekTempleIcon"
 import GridIcon from "../icons/GridIcon"
+import HTMLIcon from "../icons/HTMLIcon"
 import HappyIcon from "../icons/HappyIcon"
 import HomeIcon from "../icons/HomeIcon"
+import IcarusIcon from "../icons/IcarusIcon"
 import IceCreamIcon from "../icons/IceCreamIcon"
+import JavaScriptIcon from "../icons/JavaScriptIcon"
+import KeyIcon from "../icons/KeyIcon"
+import LadybugIcon from "../icons/LadybugIcon"
+import LeafIcon from "../icons/LeafIcon"
 import LeetCodeIcon from "../icons/LeetCodeIcon"
+import LightbulbIcon from "../icons/LightbulbIcon"
+import LightningIcon from "../icons/LightningIcon"
 import LinkedinIcon from "../icons/LinkedinIcon"
 import MagazineIcon from "../icons/MagazineIcon"
 import MagicWandIcon from "../icons/MagicWandIcon"
 import MapIcon from "../icons/MapIcon"
 import MapPinIcon from "../icons/MapPinIcon"
+import MayanPyramidIcon from "../icons/MayanPyramidIcon"
 import McDonaldsIcon from "../icons/McDonaldsIcon"
+import MedalIcon from "../icons/MedalIcon"
 import MegaphoneIcon from "../icons/MegaphoneIcon"
+import MenIcon from "../icons/MenIcon"
+import MeteorIcon from "../icons/MeteorIcon"
 import MicrosoftIcon from "../icons/MicrosoftIcon"
+import MoaiIcon from "../icons/MoaiIcon"
 import MoonIcon from "../icons/MoonIcon"
 import MoreIcon from "../icons/MoreIcon"
+import MotorBikeIcon from "../icons/MotorBikeIcon"
 import NarutoIcon from "../icons/NarutoIcon"
+import OctopusIcon from "../icons/OctopusIcon"
+import PaintBucketIcon from "../icons/PaintBucketIcon"
 import PaletteIcon from "../icons/PaletteIcon"
 import PantoneIcon from "../icons/PantoneIcon"
 import PencilIcon from "../icons/PencilIcon"
 import PersonIcon from "../icons/PersonIcon"
 import PhoneIcon from "../icons/PhoneIcon"
 import PizzaIcon from "../icons/PizzaIcon"
+import PlaneIcon from "../icons/PlaneIcon"
+import PlanetIcon from "../icons/PlanetIcon"
+import PlantIcon from "../icons/PlantIcon"
+import PolaroidIcon from "../icons/PolaroidIcon"
 import PrismIcon from "../icons/PrismIcon"
 import ProfileIcon from "../icons/ProfileIcon"
+import PulseIcon from "../icons/PulseIcon"
+import PuzzleIcon from "../icons/PuzzleIcon"
+import RainIcon from "../icons/RainIcon"
 import RainbowIcon from "../icons/RainbowIcon"
+import ReactIcon from "../icons/ReactIcon"
+import RestTimeIcon from "../icons/RestTimeIcon"
 import RioDeJaneiroIcon from "../icons/RioDeJaneiroIcon"
+import RobotIcon from "../icons/RobotIcon"
+import RocketIcon from "../icons/RocketIcon"
 import SadIcon from "../icons/SadIcon"
+import SchrodingersCatAliveIcon from "../icons/SchrodingersCatAliveIcon"
+import SchrodingersCatDeadIcon from "../icons/SchrodingersCatDeadIcon"
 import ShadowIcon from "../icons/ShadowIcon"
+import SharkIcon from "../icons/SharkIcon"
+import ShipIcon from "../icons/ShipIcon"
+import ShrimpIcon from "../icons/ShrimpIcon"
+import SmartPhoneIcon from "../icons/SmartPhoneIcon"
+import SnowFlakeIcon from "../icons/SnowFlakeIcon"
 import SonicIcon from "../icons/SonicIcon"
+import SparklingIcon from "../icons/SparklingIcon"
 import SpotifyIcon from "../icons/SpotifyIcon"
+import SpyIcon from "../icons/SpyIcon"
 import StarIcon from "../icons/StarIcon"
 import SunIcon from "../icons/SunIcon"
 import SuperHappyIcon from "../icons/SuperHappyIcon"
 import TelevisionIcon from "../icons/TelevisionIcon"
 import TerminalErrorIcon from "../icons/TerminalErrorIcon"
 import TerminalIcon from "../icons/TerminalIcon"
+import TestTubeIcon from "../icons/TestTubeIcon"
 import ThreeDIcon from "../icons/ThreeDIcon"
 import ToolsIcon from "../icons/ToolsIcon"
+import TornadoIcon from "../icons/TornadoIcon"
 import ToyBrickIcon from "../icons/ToyBrickIcon"
 import TrademarkIcon from "../icons/TrademarkIcon"
+import TrainIcon from "../icons/TrainIcon"
 import TranslateIcon from "../icons/TranslateIcon"
+import TravestiIcon from "../icons/TravestiIcon"
+import TruckIcon from "../icons/TruckIcon"
 import TumblrIcon from "../icons/TumblrIcon"
+import UmbrellaIcon from "../icons/UmbrellaIcon"
+import UnicornIcon from "../icons/UnicornIcon"
+import VHSIcon from "../icons/VHSIcon"
+import VenusOfWillendorfIcon from "../icons/VenusOfWillendorfIcon"
+import VoiceRecognitionIcon from "../icons/VoiceRecognitionIcon"
+import WalkIcon from "../icons/WalkIcon"
 import WebsiteIcon from "../icons/WebsiteIcon"
 import WhatsAppIcon from "../icons/WhatsAppIcon"
 import WikiPediaIcon from "../icons/WikiPediaIcon"
+import WindyIcon from "../icons/WindyIcon"
+import WomenIcon from "../icons/WomenIcon"
 import YouTubeIcon from "../icons/YouTubeIcon"
 
 const icons = new Map([
+    ["alien", AlienIcon],
+    ["ancientgate", AncientGateIcon],
+    ["ancientpavilion", AncientPavilionIcon],
+    ["atlas", AtlasIcon],
+    ["axolotl", AxolotlIcon],
     ["baidu", BaiduIcon],
     ["bankcard", BankCardIcon],
     ["barbie", BarbieIcon],
+    ["bastet", BastetIcon],
     ["bee", BeeIcon],
+    ["beer", BeerIcon],
+    ["bike", BikeIcon],
+    ["bowl", BowlIcon],
+    ["brain", BrainIcon],
     ["brasil", BrasilIcon],
     ["braziliancalendar", BrazilianCalendarIcon],
+    ["bread", BreadIcon],
     ["brokenlink", BrokenLinkIcon],
     ["bug", BugIcon],
     ["burger", BurgerIcon],
+    ["bus", BusIcon],
+    ["butterfly", ButterflyIcon],
+    ["css", CSSIcon],
+    ["cactus", CactusIcon],
+    ["cake", CakeIcon],
     ["calendar", CalendarIcon],
+    ["candle", CandleIcon],
+    ["capsule", CapsuleIcon],
+    ["car", CarIcon],
+    ["cat", CatIcon],
+    ["caterpillar", CaterpillarIcon],
     ["cedar", CedarIcon],
+    ["centaur", CentaurIcon],
     ["chart", ChartIcon],
+    ["cigarette", CigaretteIcon],
     ["circle", CircleIcon],
     ["clapperboard", ClapperboardIcon],
+    ["cloud", CloudIcon],
     ["code", CodeIcon],
     ["colorfilter", ColorFilterIcon],
     ["computer", ComputerIcon],
+    ["crab", CrabIcon],
     ["crown", CrownIcon],
     ["crying", CryingIcon],
+    ["cupcake", CupcakeIcon],
+    ["dna", DNAIcon],
+    ["dice", DiceIcon],
+    ["disc", DiscIcon],
+    ["djinn", DjinnIcon],
     ["download", DownloadIcon],
     ["drink", DrinkIcon],
+    ["duck", DuckIcon],
+    ["earth", EarthIcon],
+    ["egyptiancat", EgyptianCatIcon],
+    ["egyptianpyramids", EgyptianPyramidsIcon],
+    ["egyptiansphinx", EgyptianSphinxIcon],
+    ["egyptianwalk", EgyptianWalkIcon],
+    ["elephant", ElephantIcon],
     ["email", EmailIcon],
+    ["empathize", EmpathizeIcon],
     ["error", ErrorIcon],
     ["external", ExternalIcon],
+    ["eyeofhorus", EyeOfHorusIcon],
     ["facebook", FacebookIcon],
+    ["film", FilmIcon],
+    ["fire", FireIcon],
     ["firefox", FirefoxIcon],
+    ["flag", FlagIcon],
+    ["flower", FlowerIcon],
     ["gamepad", GamepadIcon],
+    ["genderless", GenderlessIcon],
+    ["ghost", GhostIcon],
     ["gitbranch", GitBranchIcon],
     ["github", GithubIcon],
+    ["glasses", GlassesIcon],
+    ["globe", GlobeIcon],
     ["google", GoogleIcon],
+    ["graduationcap", GraduationCapIcon],
+    ["greeksphinx", GreekSphinxIcon],
+    ["greektemple", GreekTempleIcon],
     ["grid", GridIcon],
+    ["html", HTMLIcon],
     ["happy", HappyIcon],
     ["home", HomeIcon],
+    ["icarus", IcarusIcon],
     ["icecream", IceCreamIcon],
+    ["javascript", JavaScriptIcon],
+    ["key", KeyIcon],
+    ["ladybug", LadybugIcon],
+    ["leaf", LeafIcon],
     ["leetcode", LeetCodeIcon],
+    ["lightbulb", LightbulbIcon],
+    ["lightning", LightningIcon],
     ["linkedin", LinkedinIcon],
     ["magazine", MagazineIcon],
     ["magicwand", MagicWandIcon],
     ["map", MapIcon],
     ["mappin", MapPinIcon],
+    ["mayanpyramid", MayanPyramidIcon],
     ["mcdonalds", McDonaldsIcon],
+    ["medal", MedalIcon],
     ["megaphone", MegaphoneIcon],
+    ["men", MenIcon],
+    ["meteor", MeteorIcon],
     ["microsoft", MicrosoftIcon],
+    ["moai", MoaiIcon],
     ["moon", MoonIcon],
     ["more", MoreIcon],
+    ["motorbike", MotorBikeIcon],
     ["naruto", NarutoIcon],
+    ["octopus", OctopusIcon],
+    ["paintbucket", PaintBucketIcon],
     ["palette", PaletteIcon],
     ["pantone", PantoneIcon],
     ["pencil", PencilIcon],
     ["person", PersonIcon],
     ["phone", PhoneIcon],
     ["pizza", PizzaIcon],
+    ["plane", PlaneIcon],
+    ["planet", PlanetIcon],
+    ["plant", PlantIcon],
+    ["polaroid", PolaroidIcon],
     ["prism", PrismIcon],
     ["profile", ProfileIcon],
+    ["pulse", PulseIcon],
+    ["puzzle", PuzzleIcon],
+    ["rain", RainIcon],
     ["rainbow", RainbowIcon],
+    ["react", ReactIcon],
+    ["resttime", RestTimeIcon],
     ["riodejaneiro", RioDeJaneiroIcon],
+    ["robot", RobotIcon],
+    ["rocket", RocketIcon],
     ["sad", SadIcon],
+    ["schrodingerscatalive", SchrodingersCatAliveIcon],
+    ["schrodingerscatdead", SchrodingersCatDeadIcon],
     ["shadow", ShadowIcon],
+    ["shark", SharkIcon],
+    ["ship", ShipIcon],
+    ["shrimp", ShrimpIcon],
+    ["smartphone", SmartPhoneIcon],
+    ["snowflake", SnowFlakeIcon],
     ["sonic", SonicIcon],
+    ["sparkling", SparklingIcon],
     ["spotify", SpotifyIcon],
+    ["spy", SpyIcon],
     ["star", StarIcon],
     ["sun", SunIcon],
     ["superhappy", SuperHappyIcon],
     ["television", TelevisionIcon],
     ["terminalerror", TerminalErrorIcon],
     ["terminal", TerminalIcon],
+    ["testtube", TestTubeIcon],
     ["threed", ThreeDIcon],
     ["tools", ToolsIcon],
+    ["tornado", TornadoIcon],
     ["toybrick", ToyBrickIcon],
     ["trademark", TrademarkIcon],
+    ["train", TrainIcon],
     ["translate", TranslateIcon],
+    ["travesti", TravestiIcon],
+    ["truck", TruckIcon],
     ["tumblr", TumblrIcon],
+    ["umbrella", UmbrellaIcon],
+    ["unicorn", UnicornIcon],
+    ["vhs", VHSIcon],
+    ["venusofwillendorf", VenusOfWillendorfIcon],
+    ["voicerecognition", VoiceRecognitionIcon],
+    ["walk", WalkIcon],
     ["website", WebsiteIcon],
     ["whatsapp", WhatsAppIcon],
     ["wikipedia", WikiPediaIcon],
+    ["windy", WindyIcon],
+    ["women", WomenIcon],
     ["youtube", YouTubeIcon]
 ])
 
---
+++ src/icons/PulseIcon.tsx
@@ -0,0 +1,23 @@
+import { IconWrapper, Icon } from "./Styles"
+import SVGProps from "../types/SVGProps"
+
+//Comment icon name (and pack if it changes)
+const PulseIcon = (props: SVGProps) => {
+	const { alt, size } = props
+	return (
+		<IconWrapper>
+			<Icon
+				$size={size}
+				role="img"
+				fill="currentColor"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+			>
+				<title>{alt}</title>
+				<path d="M9 7.53861L15 21.5386L18.6594 13H23V11H17.3406L15 16.4614L9 2.46143L5.3406 11H1V13H6.6594L9 7.53861Z"></path>
+			</Icon>
+		</IconWrapper>
+	)
+}
+
+export default PulseIcon
