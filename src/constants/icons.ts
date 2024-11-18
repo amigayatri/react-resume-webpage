18dcb28fdc8f69ef224a58a5f71b713eba1a48bd
Ami Gayatri <miragayatri@gmail.com>
2024-11-18 20:23:05 -0300

    Typo fix

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -1,6 +1,6 @@
 import { icons } from "../components/common/SVGIcon/constants"
 
-type compareKey = (a: string, b:string) => number
+type compareKeys = (a: string, b:string) => number
 const compareKeys: compareKeys = (a, b) => a.localeCompare(b)
 
 export const iconsIds = Array.from(icons.keys()).sort(compareKeys)
