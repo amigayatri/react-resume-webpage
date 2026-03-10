6cd45504460a90f8694a4fe1169218682e886589
amigayatri <miragayatri@gmail.com>
2026-03-10 20:38:21 -0300

    Dynamic Path on Pet Tag page

---
+++ src/components/pet-tag/pet/types.ts
@@ -0,0 +1,8 @@
+import { ElementProps, BaseElementProps } from "../../../types/common";
+
+export interface PetTagProps extends ElementProps {}
+export interface PetTagBaseProps extends BaseElementProps {}
+export interface PetTagBaseProps {}
+export interface PetTagMainProps extends ElementProps {
+    path: string;
+}
